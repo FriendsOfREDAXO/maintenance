1c782938598845093674bc6ddb0d86196e5e7104
Thomas Skerbis <thomas.skerbis@mac.com>
2017-09-28 00:16:30 +0200

    license info

--- pages/help.php
+++ pages/help.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * This file is part of the maintenance package.
+ *
+ * @author (c) Friends Of REDAXO
+ * @author <friendsof@redaxo.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 $file = rex_file::get(rex_path::addon('maintenance','README.md'));
 $body = rex_markdown::factory()->parse($file);
 $fragment = new rex_fragment();
