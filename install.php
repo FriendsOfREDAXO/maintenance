59a81322ce3bb70cc65c0c746a6839348ed58e4f
Thomas Skerbis <thomas.skerbis@mac.com>
2017-09-28 00:14:38 +0200

    License info

--- install.php
+++ install.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of the maintenance package.
+ *
+ * @author (c) Friends Of REDAXO
+ * @author <friendsof@redaxo.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 if (!$this->hasConfig()) {
     $this->setConfig('ip', '');
     $this->setConfig('frontend_aktiv', 'Deaktivieren');
