a7406a03dd9ce1f4e9bc129efe8126dd6ce9a56f
Thomas Skerbis <thomas.skerbis@mac.com>
2019-06-28 15:45:56 +0200

    Default Maintenance_Page as Fragment

---
+++ fragment/maintenance_page.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Maintenance</title>
+    <style type="text/css">
+        html, body {
+            height: 100%;
+            background-color: #f7f7f7;
+        }
+        body {
+            display: flex;
+            align-items: center;
+        }
+        .maintenance-container {
+            max-width: 500px;
+            min-width: 300px;
+            width: 50%;
+            margin: 0 auto;
+            color: #999;
+            font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", Arial, sans-serif;
+            font-size: 15px;
+            line-height: 1.5;
+            text-align: center;
+        }
+        .maintenance-error a {
+            color: #666;
+        }
+        .maintenance-error a:hover {
+            color: #111;
+        }
+        .maintenance-error-title {
+            margin: 0;
+            font-size: 50px;
+            font-weight: 700;
+            color: #5b98d7;
+            text-shadow: 0 4px 2px rgba(255, 255, 255, 1);
+        }
+        .maintenance-error-message {
+            padding: 0 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="maintenance-container">
+        <div class="maintenance-error">
+            <p class="maintenance-error-title">Maintenance mode</p>
+            <p class="maintenance-error-message">This website is temporarily unavailable</p>
+            <p class="maintenance-error-message">Diese Website ist temporär nicht erreichbar</p>
+        </div>
+    </div>
+</body>
+</html>'
