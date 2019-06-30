c09122a59620601737f27a6a364fd4a1d39fce44
Thomas Skerbis <thomas.skerbis@mac.com>
2019-06-30 13:11:29 +0200

    Backend Sperrung

---
+++ fragments/maintenance_page_be.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Maintenance</title>
+    <style type="text/css">
+        html, body {
+            height: 100%;
+            background-color: #f7f7f7;
+        }
+        body {
+            display: flex;
+            align-items: center;
+        }
+        .maintenance-container {
+            max-width: 500px;
+            min-width: 300px;
+            width: 50%;
+            margin: 0 auto;
+            color: #999;
+            font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", Arial, sans-serif;
+            font-size: 15px;
+            line-height: 1.5;
+            text-align: center;
+        }
+        .maintenance-error a {
+            color: #666;
+        }
+        .maintenance-error a:hover {
+            color: #111;
+        }
+        .maintenance-error-title {
+            margin: 0;
+            font-size: 40px;
+            font-weight: 700;
+            color: #5b98d7;
+            text-shadow: 0 4px 2px rgba(255, 255, 255, 1);
+            line-height: 1.2em;
+        }
+        .maintenance-error-message {
+            padding: 0 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="maintenance-container">
+        <div class="maintenance-error">
+            <p class="maintenance-error-title">Maintenance<br>Wartung</p>
+            <p class="maintenance-error-message">Backend access has been blocked, please contact your administrator.</p>
+            <p class="maintenance-error-message">Der Backend-Zugang wurde gesperrt, bitte kontaktieren Sie ihren Administrator.</p>
+        </div>
+    </div>
+</body>
+</html>'
+
