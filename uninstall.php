24e27412ca80c6496496847a26cd1969ade926ec
Thomas Skerbis <thomas.skerbis@mac.com>
2022-03-11 11:32:38 +0100

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -3,8 +3,8 @@
 $config_file = rex_path::coreData('config.yml');
 if ($config = rex_file::get($config_file)) {
     $data = rex_string::yamlDecode($config);
-    if (in_array("maintenance", $data['system_addons'])) {
-      $data['system_addons'] =  array_filter($data['system_addons'] , fn($e) => !in_array($e, ['maintenance']));
+    if (in_array("maintenance", $data['setup_addons'])) {
+      $data['system_addons'] =  array_filter($data['setup_addons'] , fn($e) => !in_array($e, ['maintenance']));
       rex_file::put($config_file, rex_string::yamlEncode($data, 3));
     } 
 }
