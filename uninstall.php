5faeda0fd774fc4516b3a5a3dadfcca1e69b17e4
Thomas Skerbis <thomas.skerbis@mac.com>
2022-03-11 11:23:06 +0100

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -4,7 +4,7 @@ $config_file = rex_path::coreData('config.yml');
 if ($config = rex_file::get($config_file)) {
     $data = rex_string::yamlDecode($config);
     if (in_array("maintenance", $data['system_addons'])) {
-      $data['system_addons'] =  array_filter($data['system_addons'] , fn($e) => !in_array($e, ['mainteance']));
+      $data['system_addons'] =  array_filter($data['system_addons'] , fn($e) => !in_array($e, ['maintenance']));
       rex_file::put($config_file, rex_string::yamlEncode($data, 3));
     } 
 }
