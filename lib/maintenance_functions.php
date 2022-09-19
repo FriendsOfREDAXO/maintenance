e8ecacdc7982ff45c3bd35e3a7c552c808746003
Thomas Skerbis <thomas.skerbis@mac.com>
2022-09-19 20:34:08 +0200

    return types

--- lib/maintenance_functions.php
+++ lib/maintenance_functions.php
@@ -10,7 +10,7 @@
  */
 class maintenance_functions { 
 	
-	public function checkUrl($url) {
+	public function checkUrl($url): bool {
 		if ($url) {
 			if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) { 
 				return false;	
@@ -18,7 +18,7 @@ class maintenance_functions {
 			return true;
 		}
 	}
-	public function checkIp($ip) {
+	public function checkIp($ip): bool {
 		if($ip){
 			if (filter_var($ip, FILTER_VALIDATE_IP)) {
 	    		return true;
