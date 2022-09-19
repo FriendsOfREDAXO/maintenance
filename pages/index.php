525596746f3591e6b761dc6f2d7dcc13bcf120cd
Thomas Skerbis <thomas.skerbis@mac.com>
2022-09-19 21:18:36 +0200

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -8,8 +8,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
-echo rex_view::title($this->i18n('title')); 
+$addon = rex_addon::get('maintenance');
+echo rex_view::title($addon->i18n('title')); 
 
 //include rex_be_controller::getCurrentPageObject()->getSubPath();
 rex_be_controller::includeCurrentPageSubPath();
