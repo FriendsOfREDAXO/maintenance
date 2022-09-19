789ec05266c52aaab7b6dcd6ee62d64ddd4d04e2
Thomas Skerbis <thomas.skerbis@mac.com>
2022-09-19 22:01:51 +0200

    Update maintenance_functions.php

--- lib/maintenance_functions.php
+++ lib/maintenance_functions.php
@@ -10,12 +10,20 @@
  */
 class maintenance_functions { 
 	
-	public function checkUrl(string $url): bool {
+	public function CheckUrl(string $url): bool {
 		if ($url !== '') {
 			if (filter_var($url, FILTER_VALIDATE_URL) === FALSE) { 
 				return false;	
 			}
 			return true;
+		}
+		return false;
+	}
+	public function CheckIp(string $ip): bool {
+		if($ip !== ''){
+			if (filter_var($ip, FILTER_VALIDATE_IP)) {
+	    		return true;
+			}
 		}
 		return false;
 	}
