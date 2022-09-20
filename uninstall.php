5bb78559548e09129da63df808318b52461175e5
Thomas Skerbis <thomas.skerbis@mac.com>
2022-09-20 16:31:55 +0200

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,10 +1,11 @@
 <?php
 // remove maintenance into System-AddOns
 $config_file = rex_path::coreData('config.yml');
-if ($config = rex_file::get($config_file)) {
+$config = rex_file::get($config_file); 
+if ($config !== null) {
     $data = rex_string::yamlDecode($config);
-    if (in_array("maintenance", $data['setup_addons'])) {
-      $data['system_addons'] =  array_filter($data['setup_addons'] , fn($e) => !in_array($e, ['maintenance']));
+    if (in_array("maintenance", $data['setup_addons'],true)) {
+      $data['system_addons'] =  array_filter($data['setup_addons'] , fn($e) => !in_array($e, ['maintenance'],true));
       rex_file::put($config_file, rex_string::yamlEncode($data, 3));
     } 
 }
