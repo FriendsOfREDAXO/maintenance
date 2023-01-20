127e140f034d923c4438222ce94d9bb8b7436e64
Thorben <webmaster@slaps.de>
2023-01-20 07:54:55 +0100

    feat: add additional tests

--- tests/e2e/maintenance.js
+++ tests/e2e/maintenance.js
@@ -9,6 +9,7 @@ describe('maintenance addon testing', () => {
          * navigate to the login screen
          */
         browser.navigateTo('/redaxo/index.php');
+        browser.pause(250);
 
         /**
          * check if the login input is present
@@ -16,6 +17,7 @@ describe('maintenance addon testing', () => {
          */
         browser.assert.elementPresent('input[id=rex-id-login-user]');
         browser.sendKeys('input[id=rex-id-login-user]', 'nightwatch_username');
+        browser.pause(250);
 
         /**
          * check if the password input is present
@@ -23,20 +25,19 @@ describe('maintenance addon testing', () => {
          */
         browser.assert.elementPresent('input[id=rex-id-login-password]');
         browser.sendKeys('input[id=rex-id-login-password]', ['nightwatch_password', browser.Keys.ENTER]);
+        browser.pause(250);
     }
 
     /**
      * logout
      */
     function logout(browser) {
+        browser.pause(200);
         browser.click('.navbar-nav a.rex-logout');
-        browser.pause(250);
     }
 
     beforeEach(browser => {
         login(browser);
-
-        browser.pause(500);
 
         /**
          * check if we are logged in to the backend
@@ -62,20 +63,24 @@ describe('maintenance addon testing', () => {
          */
         browser.navigateTo('/redaxo/index.php?page=maintenance/frontend');
         browser.click('.rex-page-main button[data-id="deakt-front"]');
-        browser.pause(250);
         browser.click('.rex-page-main #bs-select-1-1');
-        browser.pause(250);
     }
 
-    it('maintenance url secret', function (browser) {
+    it('maintenance url secret - 503', function (browser) {
         activateMaintenance(browser);
 
         /**
          * set secret
          */
         browser.updateValue('input#rex-maintenance-secret-secret-we-got-a-secret', 'nightwatch_secret');
-        browser.sendKeys('input#rex-maintenance-secret-secret-we-got-a-secret', [browser.Keys.ENTER]);
-        browser.pause(250);
+
+        /**
+         * set 503 response code
+         */
+        browser.click('.rex-page-main button[data-id="responsecode"]');
+        browser.click('.rex-page-main #bs-select-3-0');
+
+        browser.click('#rex-maintenance-save');
 
         /**
          * logout
@@ -93,58 +98,47 @@ describe('maintenance addon testing', () => {
         browser.assert.elementPresent('.maintenance-container', 'maintenance-container should exist');
         browser.assert.textContains('.maintenance-error-title', 'Maintenance', 'maintenance-error-title should contain "Maintenance"');
         browser.assert.textContains('.maintenance-error-message', 'This website is temporarily unavailable', 'maintenance-error-message should contain "This website is temporarily unavailable"');
-        browser.pause(250);
         browser.perform(async () => {
             try {
-                console.log(browser.launchUrl);
                 const response = await got(browser.launchUrl, { followRedirect: false });
                 browser.assert.equal(response.statusCode, 503, 'Status code should be 503');
             } catch (error) {
-                console.log(error);
                 browser.assert.equal(error.response.statusCode, 503, 'Status code should be 503');
             }
             return true;
         });
-        browser.pause(250);
 
         /**
          * test maintenance secret
          */
         browser.navigateTo('/?secret=nightwatch_secret');
-        // browser.pause(250);
-        // browser.perform(done => {
-        //     request(this.settings.launchUrl + '?secret=nightwatch_secret', function (error, response) {
-        //         browser.assert.equal(response.statusCode, 200, 'Status code should be 200');
-        //         done()
-        //     });
-        // })
-        browser.pause(250);
+        browser.perform(async () => {
+            try {
+                const response = await got(browser.launchUrl + '?secret=nightwatch_secret', { followRedirect: false });
+                browser.assert.equal(response.statusCode, 200, 'Status code should be 200');
+            } catch (error) {
+                browser.assert.equal(error.response.statusCode, 200, 'Status code should be 200');
+            }
+            return true;
+        });
         browser.assert.not.elementPresent('.maintenance-container', 'maintenance-container should not exist');
-        browser.pause(500);
     })
 
-    it('maintenance password', function (browser) {
-        /**
-         * navigate to the settings page
-         * activate maintenance
-         */
+    it('maintenance url secret - 403', function (browser) {
         activateMaintenance(browser);
 
         /**
          * set secret
          */
         browser.updateValue('input#rex-maintenance-secret-secret-we-got-a-secret', 'nightwatch_secret');
-        browser.pause(250);
 
         /**
-         * activate password
+         * set 403 response code
          */
-        browser.click('.rex-page-main button[data-id="type"]');
-        browser.pause(250);
-        browser.click('.rex-page-main #bs-select-2-1');
-        browser.pause(250);
+        browser.click('.rex-page-main button[data-id="responsecode"]');
+        browser.click('.rex-page-main #bs-select-3-1');
+
         browser.click('#rex-maintenance-save');
-        browser.pause(250);
 
         /**
          * logout
@@ -156,22 +150,142 @@ describe('maintenance addon testing', () => {
          */
         browser.navigateTo(this.settings.launchUrl);
 
-        browser.pause(5000);
+        /**
+         * test if maintenance is active
+         */
+        browser.assert.elementPresent('.maintenance-container', 'maintenance-container should exist');
+        browser.assert.textContains('.maintenance-error-title', 'Maintenance', 'maintenance-error-title should contain "Maintenance"');
+        browser.assert.textContains('.maintenance-error-message', 'This website is temporarily unavailable', 'maintenance-error-message should contain "This website is temporarily unavailable"');
+        browser.perform(async () => {
+            try {
+                const response = await got(browser.launchUrl, { followRedirect: false });
+                browser.assert.equal(response.statusCode, 403, 'Status code should be 403');
+            } catch (error) {
+                browser.assert.equal(error.response.statusCode, 403, 'Status code should be 403');
+            }
+            return true;
+        });
+
+        /**
+         * test maintenance secret
+         */
+        browser.navigateTo('/?secret=nightwatch_secret');
+        browser.perform(async () => {
+            try {
+                const response = await got(browser.launchUrl + '?secret=nightwatch_secret', { followRedirect: false });
+                browser.assert.equal(response.statusCode, 200, 'Status code should be 200');
+            } catch (error) {
+                browser.assert.equal(error.response.statusCode, 200, 'Status code should be 200');
+            }
+            return true;
+        });
+        browser.assert.not.elementPresent('.maintenance-container', 'maintenance-container should not exist');
+    })
+
+    it('maintenance password - 503', function (browser) {
+        /**
+         * navigate to the settings page
+         * activate maintenance
+         */
+        activateMaintenance(browser);
+
+        /**
+         * set secret
+         */
+        browser.updateValue('input#rex-maintenance-secret-secret-we-got-a-secret', 'nightwatch_secret');
+
+        /**
+         * set 503 response code
+         */
+        browser.click('.rex-page-main button[data-id="responsecode"]');
+        browser.click('.rex-page-main #bs-select-3-0');
+
+        /**
+         * activate password
+         */
+        browser.click('.rex-page-main button[data-id="type"]');
+        browser.click('.rex-page-main #bs-select-2-1');
+        browser.click('#rex-maintenance-save');
+
+        /**
+         * logout
+         */
+        logout(browser);
+
+        /**
+         * navigate to the frontend
+         */
+        browser.navigateTo(this.settings.launchUrl);
+
         // /**
         //  * test if maintenance is active
         //  */
-        browser.assert.elementPresent('input.maintenance-pw-input', 'maintenance-pw-input should exist');
-        browser.updateValue('input.maintenance-pw-input', 'nightwatch_secret');
-        browser.sendKeys('input.maintenance-pw-input', [browser.Keys.ENTER]);
-        browser.pause(250);
-        // browser.perform(done => {
-        //     request(this.settings.launchUrl, function (error, response) {
-        //         browser.assert.equal(response.statusCode, 200, 'Status code should be 200');
-        //         done()
-        //     });
-        // })
-        // browser.pause(250);
-        browser.assert.not.elementPresent('input.maintenance-pw-input', 'maintenance-pw-input should not exist');
+        browser.assert.elementPresent('.maintenance-pw-input', 'maintenance-pw-input should exist');
+        browser.perform(async () => {
+            try {
+                const response = await got(browser.launchUrl, { followRedirect: false });
+                browser.assert.equal(response.statusCode, 503, 'Status code should be 503');
+            } catch (error) {
+                browser.assert.equal(error.response.statusCode, 503, 'Status code should be 503');
+            }
+            return true;
+        });
+        browser.updateValue('.maintenance-pw-input', 'nightwatch_secret');
+        browser.sendKeys('.maintenance-pw-input', [browser.Keys.ENTER]);
+        browser.assert.not.elementPresent('.maintenance-pw-input', 'maintenance-pw-input should not exist');
+    })
+
+    it('maintenance password - 403', function (browser) {
+        /**
+         * navigate to the settings page
+         * activate maintenance
+         */
+        activateMaintenance(browser);
+
+        /**
+         * set secret
+         */
+        browser.updateValue('input#rex-maintenance-secret-secret-we-got-a-secret', 'nightwatch_secret');
+
+        /**
+         * set 403 response code
+         */
+        browser.click('.rex-page-main button[data-id="responsecode"]');
+        browser.click('.rex-page-main #bs-select-3-1');
+
+        /**
+         * activate password
+         */
+        browser.click('.rex-page-main button[data-id="type"]');
+        browser.click('.rex-page-main #bs-select-2-1');
+        browser.click('#rex-maintenance-save');
+
+        /**
+         * logout
+         */
+        logout(browser);
+
+        /**
+         * navigate to the frontend
+         */
+        browser.navigateTo(this.settings.launchUrl);
+
+        // /**
+        //  * test if maintenance is active
+        //  */
+        browser.assert.elementPresent('.maintenance-pw-input', 'maintenance-pw-input should exist');
+        browser.perform(async () => {
+            try {
+                const response = await got(browser.launchUrl, { followRedirect: false });
+                browser.assert.equal(response.statusCode, 403, 'Status code should be 403');
+            } catch (error) {
+                browser.assert.equal(error.response.statusCode, 403, 'Status code should be 403');
+            }
+            return true;
+        });
+        browser.updateValue('.maintenance-pw-input', 'nightwatch_secret');
+        browser.sendKeys('.maintenance-pw-input', [browser.Keys.ENTER]);
+        browser.assert.not.elementPresent('.maintenance-pw-input', 'maintenance-pw-input should not exist');
     })
 
     /**
@@ -189,6 +303,7 @@ describe('maintenance addon testing', () => {
          * reset settings
          */
         browser.navigateTo('/redaxo/index.php?page=maintenance/frontend');
+        browser.pause(200);
         browser.updateValue('input#rex-maintenance-secret-secret-we-got-a-secret', '');
         browser.sendKeys('input#rex-maintenance-secret-secret-we-got-a-secret', [browser.Keys.ENTER]);
         browser.pause(250);
