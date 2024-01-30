9914b97f27671194d6a0fa80065b479230011d8d
Thomas Skerbis <thomas.skerbis@mac.com>
2024-01-30 16:05:53 +0100

    use: rex_file::putConfig and rex_file::getConfig

--- uninstall.php
+++ uninstall.php
@@ -1,11 +1,7 @@
 <?php
-// remove maintenance into System-AddOns
 $config_file = rex_path::coreData('config.yml');
-$config = rex_file::get($config_file); 
-if ($config !== null) {
-    $data = rex_string::yamlDecode($config);
-    if (in_array("maintenance", $data['setup_addons'],true)) {
-      $data['system_addons'] =  array_filter($data['setup_addons'] , fn($e) => !in_array($e, ['maintenance'],true));
-      rex_file::put($config_file, rex_string::yamlEncode($data, 3));
-    } 
+$data = rex_file::getConfig($config_file);
+if ($data && in_array('maintenance', $data['setup_addons'], true)) {
+    $data['setup_addons'] = array_filter($data['setup_addons'], fn($e) => $e !== 'maintenance');
+    rex_file::putConfig($config_file, $data);
 }
