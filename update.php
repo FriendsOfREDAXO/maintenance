787a92c95da6ab52420a5fedbacd16907f559cdd
Alexander Walther <mail@alexplus.de>
2024-07-04 02:28:37 +0200

    Migrations update.php vorbereitet

---
+++ update.php
@@ -0,0 +1,11 @@
+<?php
+
+// Upgrade from 2.x to 3.x
+
+/*
+if (rex_config::get('maintenance', 'block_frontend') === '1') {
+    rex_config::set('maintenance', 'block_frontend', true);
+} else {
+    rex_config::set('maintenance', 'block_frontend', false);
+}
+*/
