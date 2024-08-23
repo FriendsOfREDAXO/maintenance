e4b13a597a418af475798cb47d5a4f684b688ee4
Alexander Walther <github.com@alexplus.de>
2024-08-23 14:23:06 +0200

    Update announcement.php

--- fragments/maintenance/announcement.php
+++ fragments/maintenance/announcement.php
@@ -2,11 +2,12 @@
 $announcement = rex_config::get('maintenance', 'announcement', '');
 $start_date = rex_config::get('maintenance', 'announcement_start_date');
 $end_date = rex_config::get('maintenance', 'announcement_end_date');
-$current_date = date('Y-m-d H:i:s');
+$current_date = date('Y-m-d\TH:i:s');
 
 if ($announcement !== '' && $start_date <= $current_date && $end_date >= $current_date) {
     ?>
 <div class="maintenance-announcement">
     <?php echo $announcement; ?>
 </div>
-<?php } ?>
+<?php }
+?>
