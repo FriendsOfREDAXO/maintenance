2f0dea28e4657b0151f8d6cfe720b80ddaca3b0a
Simon Krull <simonkrull@mailbox.org>
2025-02-15 11:47:07 +0100

    TASK: servername auch im title tag im backend fragment ergänzt

--- fragments/maintenance/backend.php
+++ fragments/maintenance/backend.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Maintenance</title>
+    <title><?= rex::getServerName() ?> - Maintenance</title>
     <style type="text/css">
         html, body {
             height: 100%;
